88859e5eda6c4b9575f5a644b7dde23e10051249
Jose Villegas <jose123millon@gmail.com>
2024-04-03 11:55:00 -0500

    Improve The DashboardHeader

--- src/components/Dashboard/DashboardHeader.svelte
+++ src/components/Dashboard/DashboardHeader.svelte
@@ -1,19 +1,40 @@
+<script>
+</script>
+
 <header>
-	<h1>
-		{new Date().getHours() < 12
-			? "Buenos Dias"
-			: new Date().getHours() < 18
-				? "Buenas Tardes"
-				: "Buenas Noches"}
-	</h1>
-	<p>Aqui tienes los resumenes de hoy</p>
+	<header>
+		<h1>
+			{new Date().getHours() < 12
+				? "Buenos Dias"
+				: new Date().getHours() < 18
+					? "Buenas Tardes"
+					: "Buenas Noches"}
+		</h1>
+	</header>
+
+	<footer>
+		<p>Aqui tienes los resumenes de hoy</p>
+	</footer>
 </header>
 
 <style>
-	header {
+	header:not(header > header) {
 		background-color: var(--color);
 		padding: 1rem;
 		border-radius: var(--radius);
 		margin-bottom: 1rem;
+	}
+
+	header > header {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+	}
+
+	button {
+		display: flex;
+		align-items: center;
+		gap: 0.5rem;
+		font-size: larger;
 	}
 </style>
